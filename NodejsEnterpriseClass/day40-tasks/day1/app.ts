ce02d53320a87be1f416ec8644a6340b8c454d52
Benson\Benson <a092791097@gmail.com>
2023-03-10 10:37:37 +0800

    Day 1 - 非同步概念、建立 Promise

---
+++ NodejsEnterpriseClass/day40-tasks/day1/app.ts
@@ -0,0 +1,18 @@
+const checkScore = () => {
+  return new Promise((resolve, reject) => {
+    /* 回傳一個 Promise，並執行以下非同步操作*/
+    const score = Math.round(Math.random() * 100);
+
+    setTimeout(() => {
+      if (score >= 60) {
+        resolve(score); // 執行實現方法
+      } else {
+        reject(score); // 執行拒絕方法
+      }
+    }, 1000);
+  });
+};
+
+checkScore()
+  .then((score) => console.log(`${score}分，及格`))
+  .catch((err) => console.log(`${err}分，不及格`));
