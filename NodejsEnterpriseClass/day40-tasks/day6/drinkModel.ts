fed87c1389c81c51b33388cad0e1f697b51d8361
ERI\ioveasdkre <a96080103A>
2023-03-13 10:53:00 +0800

    Day 6 - Mongoose、Schema

---
+++ NodejsEnterpriseClass/day40-tasks/day6/drinkModel.ts
@@ -0,0 +1,39 @@
+// Day 6 - Mongoose、Schema
+
+// https://hackmd.io/wWcoMxCYRtmtfhjDrwNgew?view
+
+import { Schema, model, Document } from "mongoose";
+
+interface IDrinks extends Document {
+  product: string;
+  price: number;
+  ice: string;
+  sugar: string;
+  toppings: string[];
+}
+
+const drinkSchema = new Schema<IDrinks>({
+  product: {
+    type: String,
+    required: [true, "產品名稱未填寫"],
+  },
+  price: {
+    type: Number,
+    required: [true, "價錢未填寫"],
+  },
+  ice: {
+    type: String,
+    default: "正常冰",
+  },
+  sugar: {
+    type: String,
+    default: "全糖",
+  },
+  toppings: {
+    type: [String],
+  },
+});
+
+const Drink = model<IDrinks>("Drink", drinkSchema);
+
+export { Drink, IDrinks };
