33e0b04fd63085693c3fd78a9f28a909b9ea9089
Benson\Benson <a283835639@gmail.com>
2023-03-30 20:53:05 +0800

    update: 移除無用註解 Day 24 - 非同步錯誤管理

--- NodejsEnterpriseClass/day40-tasks/day24/postRouters.ts
+++ NodejsEnterpriseClass/day40-tasks/day24/postRouters.ts
@@ -1,19 +1,10 @@
 import express, { Request, Response, NextFunction } from "express";
 import { handleErrorAsync } from "./handleErrorAsync";
-// import { Post } from "./post";
 
 const router = express.Router();
 
 const getPosts = async (_req: Request, _res: Response, _next: NextFunction) => {
   throw new Error("test error");
-  // const post = await Post.find();
-  // res.status(200).json({
-  //   status: "success",
-  //   results: post.length,
-  //   data: {
-  //     post,
-  //   },
-  // });
 };
 
 router.get("/", handleErrorAsync(getPosts));
